b979ab2da294fe3a0ac830ab8202faefc47dc494
Redon Gongo <redon.gongo@outlook.com>
2023-03-16 16:42:12 +0100

    Reservation Entity

---
+++ src/main/java/net/sparklab/AirBNBReservation/model/Reservation.java
@@ -0,0 +1,33 @@
+package net.sparklab.AirBNBReservation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Currency;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reservations")
+public class Reservation extends BaseEntity{
+    private Long createdDate;
+    //    private long lastModifiedDate
+    private String confirmationCode;
+    private int noAdults;
+    private int noChildren;
+    private int noInfants;
+    private Long startDate;
+    private Long endDate;
+    private int noNights;
+    private String listing;
+    private Currency earning;
+//    @OneToMany(mappedBy = "guest")
+//    private Guest guest;
+
+}
