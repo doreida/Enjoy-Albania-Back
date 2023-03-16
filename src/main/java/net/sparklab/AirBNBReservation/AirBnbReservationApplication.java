3eaa6909de265424d8b3488a8168157398f92357
blerta <blerta.prendi@sparklab.al>
2023-03-16 09:54:31 +0100

    Initial commit

---
+++ src/main/java/net/sparklab/AirBNBReservation/AirBnbReservationApplication.java
@@ -0,0 +1,13 @@
+package net.sparklab.AirBNBReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sparklab/AirBNBReservation/AirBnbReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sparklab.AirBNBReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
