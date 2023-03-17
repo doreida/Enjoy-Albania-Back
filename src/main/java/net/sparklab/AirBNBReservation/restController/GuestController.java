88a40ef73004c347f547fa3efbceb1818eabc545
doreida <62391361+doreida@users.noreply.github.com>
2023-03-17 14:18:08 +0100

    Guest Controller and Service

--- src/main/java/net/sparklab/AirBNBReservation/restController/GuestController.java
+++ src/main/java/net/sparklab/AirBNBReservation/restController/GuestController.java
@@ -1,5 +1,32 @@
 package net.sparklab.AirBNBReservation.restController;
 
+import lombok.RequiredArgsConstructor;
+import net.sparklab.AirBNBReservation.dto.GuestDTO;
+import net.sparklab.AirBNBReservation.repositories.GuestRepository;
+import net.sparklab.AirBNBReservation.services.GuestService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("AirBNBReservation/guest")
+@RequiredArgsConstructor
 public class GuestController {
+
+    private final GuestService guestService;
+    private final GuestRepository guestRepository;
+    @GetMapping("/id")
+    public GuestDTO findGuestById(@PathVariable String id){
+        return guestService.findById(id);
+    }
+    @GetMapping("/allGuests")
+    public List<GuestDTO> findAllGuests(){
+        return guestService.findAll();
+    }
+
+
+
 }
--- src/main/java/net/sparklab/AirBNBReservation/services/GuestService.java
+++ src/main/java/net/sparklab/AirBNBReservation/services/GuestService.java
@@ -1,4 +1,32 @@
 package net.sparklab.AirBNBReservation.services;
 
+import net.sparklab.AirBNBReservation.converters.GuestToGuestDTO;
+import net.sparklab.AirBNBReservation.dto.GuestDTO;
+import net.sparklab.AirBNBReservation.exceptions.NotFoundException;
+import net.sparklab.AirBNBReservation.repositories.GuestRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class GuestService {
+
+    private final GuestRepository guestRepository;
+    private final GuestToGuestDTO toGuestDTO;
+
+    public GuestService(GuestRepository guestRepository, GuestToGuestDTO toGuestDTO) {
+        this.guestRepository = guestRepository;
+        this.toGuestDTO = toGuestDTO;
+    }
+
+    public GuestDTO findById(String id){
+        Long parseId;
+        parseId=Long.parseLong(id);
+        return toGuestDTO.convert(guestRepository.findById(parseId).orElseThrow(()-> new NotFoundException("Record with id: "+ id +"not found!")));
+    }
+
+    public List<GuestDTO> findAll(){
+        return guestRepository.findAll().stream().map(guest -> toGuestDTO.convert(guest)).collect(Collectors.toList());
+    }
 }
